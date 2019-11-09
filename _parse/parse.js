d32c0c35e08ffcd9f8cda8d0f5c3a5186c61de83
charescape <charescape@outlook.com>
2019-11-09 22:55:12 +0800

    fix

--- _parse/parse.js
+++ _parse/parse.js
@@ -1,15 +1,15 @@
 (function () {
   UE = window.UE || {};
   var isIE = !!window.ActiveXObject;
-  //定义utils工具
+  // 定义utils工具
   var utils = {
     removeLastbs: function (url) {
-      return url.replace(/\/$/, '')
+      return url.replace(/\/$/, '');
     },
     extend: function (t, s) {
-      var a = arguments,
-        notCover = this.isBoolean(a[a.length - 1]) ? a[a.length - 1] : false,
-        len = this.isBoolean(a[a.length - 1]) ? a.length - 1 : a.length;
+      var a = arguments;
+      var notCover = this.isBoolean(a[a.length - 1]) ? a[a.length - 1] : false;
+      var len = this.isBoolean(a[a.length - 1]) ? a.length - 1 : a.length;
       for (var i = 1; i < len; i++) {
         var x = a[i];
         for (var k in x) {
@@ -32,7 +32,7 @@
       var sheetStyle;
       if (!indexList[key]) {
         if (style === undefined) {
-          return ''
+          return '';
         }
         sheetStyle = doc.createStyleSheet('', index = doc.styleSheets.length);
         indexList[key] = index;
@@ -40,39 +40,39 @@
         sheetStyle = doc.styleSheets[indexList[key]];
       }
       if (style === undefined) {
-        return sheetStyle.cssText
+        return sheetStyle.cssText;
       }
-      sheetStyle.cssText = sheetStyle.cssText + '\n' + (style || '')
+      sheetStyle.cssText = sheetStyle.cssText + '\n' + (style || '');
     } : function (key, style, doc) {
       doc = doc || document;
-      var head = doc.getElementsByTagName('head')[0], node;
+      var head = doc.getElementsByTagName('head')[0]; var node;
       if (!(node = doc.getElementById(key))) {
         if (style === undefined) {
-          return ''
+          return '';
         }
         node = doc.createElement('style');
         node.id = key;
-        head.appendChild(node)
+        head.appendChild(node);
       }
       if (style === undefined) {
-        return node.innerHTML
+        return node.innerHTML;
       }
       if (style !== '') {
         node.innerHTML = node.innerHTML + '\n' + style;
       } else {
-        head.removeChild(node)
+        head.removeChild(node);
       }
     },
     domReady: function (onready) {
       var doc = window.document;
-      if (doc.readyState === "complete") {
+      if (doc.readyState === 'complete') {
         onready();
       } else {
         if (isIE) {
           (function () {
             if (doc.isReady) return;
             try {
-              doc.documentElement.doScroll("left");
+              doc.documentElement.doScroll('left');
             } catch (error) {
               setTimeout(arguments.callee, 0);
               return;
@@ -80,32 +80,29 @@
             onready();
           })();
           window.attachEvent('onload', function () {
-            onready()
+            onready();
           });
         } else {
-          doc.addEventListener("DOMContentLoaded", function () {
-            doc.removeEventListener("DOMContentLoaded", arguments.callee, false);
+          doc.addEventListener('DOMContentLoaded', function () {
+            doc.removeEventListener('DOMContentLoaded', arguments.callee, false);
             onready();
           }, false);
           window.addEventListener('load', function () {
-            onready()
+            onready();
           }, false);
         }
       }
-
     },
     each: function (obj, iterator, context) {
       if (obj == null) return;
       if (obj.length === +obj.length) {
         for (var i = 0, l = obj.length; i < l; i++) {
-          if (iterator.call(context, obj[i], i, obj) === false)
-            return false;
+          if (iterator.call(context, obj[i], i, obj) === false) { return false; }
         }
       } else {
         for (var key in obj) {
           if (obj.hasOwnProperty(key)) {
-            if (iterator.call(context, obj[key], key, obj) === false)
-              return false;
+            if (iterator.call(context, obj[key], key, obj) === false) { return false; }
           }
         }
       }
@@ -122,7 +119,7 @@
     },
     pushItem: function (arr, item) {
       if (this.inArray(arr, item) == -1) {
-        arr.push(item)
+        arr.push(item);
       }
     },
     trim: function (str) {
@@ -159,70 +156,73 @@
       elm.className = utils.trim(cls);
     },
     removeClass: function (elm, classNames) {
-      classNames = this.isArray(classNames) ? classNames :
-        this.trim(classNames).replace(/[ ]{2,}/g, ' ').split(' ');
+      classNames = this.isArray(classNames) ? classNames
+        : this.trim(classNames).replace(/[ ]{2,}/g, ' ').split(' ');
       for (var i = 0, ci, cls = elm.className; ci = classNames[i++];) {
-        cls = cls.replace(new RegExp('\\b' + ci + '\\b'), '')
+        cls = cls.replace(new RegExp('\\b' + ci + '\\b'), '');
       }
       cls = this.trim(cls).replace(/[ ]{2,}/g, ' ');
       elm.className = cls;
       !cls && elm.removeAttribute('className');
     },
     on: function (element, type, handler) {
-      var types = this.isArray(type) ? type : type.split(/\s+/),
-        k = types.length;
-      if (k) while (k--) {
-        type = types[k];
-        if (element.addEventListener) {
-          element.addEventListener(type, handler, false);
-        } else {
-          if (!handler._d) {
-            handler._d = {
-              els: []
-            };
-          }
-          var key = type + handler.toString(), index = utils.indexOf(handler._d.els, element);
-          if (!handler._d[key] || index == -1) {
-            if (index == -1) {
-              handler._d.els.push(element);
-            }
-            if (!handler._d[key]) {
-              handler._d[key] = function (evt) {
-                return handler.call(evt.srcElement, evt || window.event);
+      var types = this.isArray(type) ? type : type.split(/\s+/);
+      var k = types.length;
+      if (k) {
+        while (k--) {
+          type = types[k];
+          if (element.addEventListener) {
+            element.addEventListener(type, handler, false);
+          } else {
+            if (!handler._d) {
+              handler._d = {
+                els: []
               };
             }
+            var key = type + handler.toString(); var index = utils.indexOf(handler._d.els, element);
+            if (!handler._d[key] || index == -1) {
+              if (index == -1) {
+                handler._d.els.push(element);
+              }
+              if (!handler._d[key]) {
+                handler._d[key] = function (evt) {
+                  return handler.call(evt.srcElement, evt || window.event);
+                };
+              }
 
-
-            element.attachEvent('on' + type, handler._d[key]);
+              element.attachEvent('on' + type, handler._d[key]);
+            }
           }
         }
       }
       element = null;
     },
     off: function (element, type, handler) {
-      var types = this.isArray(type) ? type : type.split(/\s+/),
-        k = types.length;
-      if (k) while (k--) {
-        type = types[k];
-        if (element.removeEventListener) {
-          element.removeEventListener(type, handler, false);
-        } else {
-          var key = type + handler.toString();
-          try {
-            element.detachEvent('on' + type, handler._d ? handler._d[key] : handler);
-          } catch (e) {
-          }
-          if (handler._d && handler._d[key]) {
-            var index = utils.indexOf(handler._d.els, element);
-            if (index != -1) {
-              handler._d.els.splice(index, 1);
+      var types = this.isArray(type) ? type : type.split(/\s+/);
+      var k = types.length;
+      if (k) {
+        while (k--) {
+          type = types[k];
+          if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+          } else {
+            var key = type + handler.toString();
+            try {
+              element.detachEvent('on' + type, handler._d ? handler._d[key] : handler);
+            } catch (e) {
             }
-            handler._d.els.length == 0 && delete handler._d[key];
+            if (handler._d && handler._d[key]) {
+              var index = utils.indexOf(handler._d.els, element);
+              if (index != -1) {
+                handler._d.els.splice(index, 1);
+              }
+              handler._d.els.length == 0 && delete handler._d[key];
+            }
           }
         }
       }
     },
-    loadFile: function () {
+    loadFile: (function () {
       var tmpList = [];
 
       function getItem (doc, obj) {
@@ -235,7 +235,6 @@
         } catch (e) {
           return null;
         }
-
       }
 
       return function (doc, obj, fn) {
@@ -244,7 +243,7 @@
           if (item.ready) {
             fn && fn();
           } else {
-            item.funs.push(fn)
+            item.funs.push(fn);
           }
           return;
         }
@@ -257,7 +256,7 @@
           var html = [];
           for (var p in obj) {
             if (p == 'tag') continue;
-            html.push(p + '="' + obj[p] + '"')
+            html.push(p + '="' + obj[p] + '"');
           }
           doc.write('<' + obj.tag + ' ' + html.join(' ') + ' ></' + obj.tag + '>');
           return;
@@ -283,16 +282,16 @@
           }
         };
         element.onerror = function () {
-          throw Error('The load ' + (obj.href || obj.src) + ' fails,check the url')
+          throw Error('The load ' + (obj.href || obj.src) + ' fails,check the url');
         };
-        doc.getElementsByTagName("head")[0].appendChild(element);
-      }
-    }()
+        doc.getElementsByTagName('head')[0].appendChild(element);
+      };
+    }())
   };
   utils.each(['String', 'Function', 'Array', 'Number', 'RegExp', 'Object', 'Boolean'], function (v) {
     utils['is' + v] = function (obj) {
       return Object.prototype.toString.apply(obj) == '[object ' + v + ']';
-    }
+    };
   });
   var parselist = {};
   UE.parse = {
@@ -302,31 +301,31 @@
     load: function (opt) {
       utils.each(parselist, function (v) {
         v.call(opt, utils);
-      })
+      });
     }
   };
   uParse = function (selector, opt) {
     utils.domReady(function () {
       var contents;
       if (document.querySelectorAll) {
-        contents = document.querySelectorAll(selector)
+        contents = document.querySelectorAll(selector);
       } else {
         if (/^#/.test(selector)) {
-          contents = [document.getElementById(selector.replace(/^#/, ''))]
+          contents = [document.getElementById(selector.replace(/^#/, ''))];
         } else if (/^\./.test(selector)) {
           var contents = [];
           utils.each(document.getElementsByTagName('*'), function (node) {
             if (node.className && new RegExp('\\b' + selector.replace(/^\./, '') + '\\b', 'i').test(node.className)) {
-              contents.push(node)
+              contents.push(node);
             }
-          })
+          });
         } else {
-          contents = document.getElementsByTagName(selector)
+          contents = document.getElementsByTagName(selector);
         }
       }
       utils.each(contents, function (v) {
-        UE.parse.load(utils.extend({root: v, selector: selector}, opt))
-      })
-    })
-  }
+        UE.parse.load(utils.extend({ root: v, selector: selector }, opt));
+      });
+    });
+  };
 })();
